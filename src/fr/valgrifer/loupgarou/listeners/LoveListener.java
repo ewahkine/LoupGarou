3c06960cfc63b4f9b45377255121fb35162f3239
Valgrifer <valgrifer123@gmail.com>
2023-07-16 02:53:35 +0200

    Fix Couple logic

--- src/fr/valgrifer/loupgarou/listeners/LoveListener.java
+++ src/fr/valgrifer/loupgarou/listeners/LoveListener.java
@@ -1,10 +1,8 @@
 package fr.valgrifer.loupgarou.listeners;
 
-import com.comphenix.packetwrapper.WrapperPlayServerEntityDestroy;
 import fr.valgrifer.loupgarou.classes.LGPlayer;
 import fr.valgrifer.loupgarou.classes.LGWinType;
 import fr.valgrifer.loupgarou.events.*;
-import fr.valgrifer.loupgarou.roles.RCupid;
 import fr.valgrifer.loupgarou.roles.RoleWinType;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
@@ -13,7 +11,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.Collectors;
 
 import static fr.valgrifer.loupgarou.utils.ChatColorQuick.*;
@@ -37,61 +34,25 @@ public class LoveListener implements Listener {
 
     @EventHandler(priority = EventPriority.HIGH)
     public void onGameEnd(LGGameEndEvent e) {
-        WrapperPlayServerEntityDestroy destroy = new WrapperPlayServerEntityDestroy();
-        List<Integer> ids = new ArrayList<>();
-
-        for(LGPlayer lgp : e.getGame().getInGame())
-            ids.add(Integer.MAX_VALUE-lgp.getPlayer().getEntityId());
-
-        int[] intList = new int[ids.size()];
-
-        for(int i = 0;i<ids.size();i++)
-            intList[i] = ids.get(i);
-
-        destroy.setEntityIds(intList);
-
-        for(LGPlayer lgp : e.getGame().getInGame())
-            destroy.sendPacket(lgp.getPlayer());
-
         if(e.getWinType() == LGWinType.COUPLE)
-            e.getWinners().addAll(e.getGame().getAlive());
-        else
-            e.getWinners().removeAll(e.getWinners().stream()
-                .filter(player -> player.getCache().has(loveKey))
-                .collect(Collectors.toCollection(ArrayList::new)));
-    }
-
-    @EventHandler
-    public void onEndCheck(LGEndCheckEvent e) {
-        if(e.getGame().getAlive(lgPlayer -> lgPlayer.getRoleWinType() != RoleWinType.NONE).size() > 3)
-            return;
-
-        boolean cupidonAlive = false;
-        int manyCoupleAlive = 0;
-
-        for(LGPlayer lgp : e.getGame().getAlive())
-            if (lgp.getRole() instanceof RCupid)
-                cupidonAlive = true;
-            else if(lgp.getCache().has(loveKey))
-                manyCoupleAlive++;
-
-        boolean coupleAlive = manyCoupleAlive == 2;
-
-        if(e.getGame().getAlive().size() == ((coupleAlive ? 2 : 0) + (cupidonAlive ? 1 : 0)))
-            e.setWinType(LGWinType.COUPLE);
+            e.getWinners().addAll(e.getGame().getInGame().stream()
+                    .filter(lgp -> lgp.getRoleWinType() == RoleWinType.COUPLE)
+                    .collect(Collectors.toCollection(ArrayList::new)));
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onChat(AsyncPlayerChatEvent e) {
         LGPlayer player = LGPlayer.thePlayer(e.getPlayer());
-        if(e.getMessage().startsWith("!")) {
-            e.setCancelled(true);
-            if(player.getCache().has(loveKey))
-            {
-                player.sendMessage(LIGHT_PURPLE+"❤ Vous "+GOLD+"» "+WHITE+e.getMessage().substring(1));
-                player.getCache().<LGPlayer>get(loveKey).sendMessage(LIGHT_PURPLE+"❤ Votre Amoureux "+GOLD+"» "+WHITE+e.getMessage().substring(1));
-            }
-        }
+        if(!e.getMessage().startsWith("!"))
+            return;
+
+        e.setCancelled(true);
+
+        if(!player.getCache().has(loveKey))
+            return;
+
+        player.sendMessage(LIGHT_PURPLE+"❤ Vous "+GOLD+"» "+WHITE+e.getMessage().substring(1));
+        player.getCache().<LGPlayer>get(loveKey).sendMessage(LIGHT_PURPLE+"❤ Votre Amoureux "+GOLD+"» "+WHITE+e.getMessage().substring(1));
     }
     @EventHandler(priority = EventPriority.HIGH)
     public void onUpdatePrefix (LGUpdatePrefixEvent e) {
